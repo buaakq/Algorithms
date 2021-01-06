9604f413e5dcc5c2df6d9043d3dce4acad47a879
qiaokang92 <qiaokang1213@gmail.com>
2021-01-06 12:29:57 -0600

    dfs in python

--- python/graph.py
+++ python/graph.py
@@ -6,9 +6,30 @@ class Graph:
    def __init__(self, graph):
       self.graph = graph
 
-   def dfs(self):
-      print("dfs the graph:")
-      pass
+   def dfs(self, start):
+      print("dfs:")
+      visited = set()
+      stack = [start]
+      while len(stack) > 0:
+         nextNode = stack.pop()
+         if nextNode not in visited:
+            # visit this node
+            print(nextNode)
+            visited.add(nextNode)
+            # add all child nodes which are not visited to the stack
+            stack.extend(self.graph[nextNode] - visited)
+      return visited
+
+   def dfs_recursive(self, start, visited=None):
+      if visited == None:
+         print("dfs recursive:")
+         visited = set()
+      visited.add(start)
+      print(start)
+      for nextNode in self.graph[start]:
+         if nextNode not in visited:
+            self.dfs_recursive(nextNode, visited)
+      return visited
 
    def bfs(self):
       print("bfs the graph:")
@@ -29,5 +50,6 @@ if __name__ == "__main__":
                  'F': set(['C', 'E'])}
 
    graph = Graph(graph_dict)
-   graph.dfs()
+   #graph.dfs('A')
+   graph.dfs_recursive('A')
    graph.bfs()
